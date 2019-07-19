ae8d5f286c0a7cc56797815a8fed32776bd01035
G-Chilie <okonkwolinda7@gmail.com>
2019-07-19 18:28:07 +0100

    change error massage

--- app/helpers/index.js
+++ app/helpers/index.js
@@ -4,24 +4,6 @@ class Helpers {
       errors.map(error => validationErrors.push(error.msg));
       return validationErrors;
     }
-  
-    static isANumber(num) {
-      return Number.isInteger(Number(num));
-    }
-  
-    static updateType(req, res, type, data, id, name) {
-      for (let i = 0; i < type.length; i += 1) {
-        if (type[i].id === id) {
-          type.splice(i, 1);
-          type.push(data);
-          return res.status(200).json({
-            status: 200,
-            data: [data],
-            message: `${name} Ad updated successfully`,
-          });
-        }
-      }
-    }
 }
 
 export default Helpers;
